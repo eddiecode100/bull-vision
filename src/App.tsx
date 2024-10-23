01f7d0424b12c06778f7e13303ee88847178c0db
eddiecode100 <eddiecode100@gmail.com>
2024-10-23 08:40:11 -0700

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, FormEvent } from "react";
+import axios from "axios";
+
+const POLYGON_API_KEY = import.meta.env.VITE_POLYGON_API_KEY;
+
+function App() {
+  return <></>;
+}
+
+export default App;
